4ee3162ba64826e8dc0e202aed807c4b45758b50
Xilllo-an <Xitan6@s.sfusd.edu>
2026-03-24 22:03:53 -0700

    Lives, Collect Coins, Level 2, and Home

---
+++ menu.gd
@@ -0,0 +1,8 @@
+extends Control
+
+func _on_play_butron_pressed() -> void:
+	PlayerStats.score = 0
+	get_tree().change_scene_to_file("res://Scenes/leve_1.tscn")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
